d20c8c256c1936f08c3bb6c756fb985efb38c3a9
amith-webdeveloper <amithwebdev1840@gmail.com>
2024-07-26 17:11:35 +0530

    day01

---
+++ index.ts
@@ -0,0 +1,24 @@
+// console.log("learning typescript");
+
+// types
+// primitives and reference
+
+// number boolean undefined string bigint null are primitves
+// arrays functions objects tuples classes  are references 
+
+/* Basic types 
+- Number , String , Boolean
+- Arrays , Tuples
+- Any , Unknown , Never , Void 
+- Enums
+*/
+
+let a:number;
+let arr:[]
+let str:string;
+
+let arr2:[number, string , boolean] = [2 , "amith" , false]
+
+
+
+
